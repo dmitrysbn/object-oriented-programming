1e862d3ac1fc3afbfffcd94d5739e961a44299f3
dmitrysbn <dmitry.sbn@gmail.com>
2018-02-03 15:01:15 -0500

    papermandem

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -1,11 +1,58 @@
 class Paperboy
 
-  def initialize(name, experience, earnings)
+  def initialize(name)
     @name = name
-    @experience = experience
-    @earnings = earnings
+    @experience = 0
+    @earnings = 0
   end
 
-  def quota
-    
+  def name
+    @name
   end
+
+  def experience
+    @experience
+  end
+
+  def earnings
+    @earnings
+  end
+
+  ### INSTANCE METHODS ###
+
+  def quota
+    50 + @experience/2
+  end
+
+  def deliver(start_address, end_address)
+    length = (start_address - end_address).abs + 1
+
+    if length < quota
+      @earnings += length * 0.25 - 2
+      @experience += length
+      length * 0.25 - 2
+    else
+      @earnings += quota * 0.25 + (length - quota) * 0.50
+      @experience += length
+      quota * 0.25 + (length - quota) * 0.50
+    end
+  end
+
+  def report
+    puts "I'm #{@name}, I've delivered #{@experience} papers and I've earned $#{@earnings} so far!"
+  end
+
+end
+
+peter = Paperboy.new("Peter")
+
+puts peter.quota
+puts peter.deliver(160, 101)
+# puts peter.deliver(160, 101)
+puts peter.earnings
+peter.report
+
+puts peter.quota
+puts peter.deliver(1, 75)
+puts peter.earnings
+peter.report
